acaf2951df728c5f4b356f5ad9cc7987cf1cee93
blaze1522 <chakrabarty.goirik@gmail.com>
2020-05-12 16:09:32 +0530

    Uploading Insta scraper

---
+++ insta_scrap.py
@@ -0,0 +1,47 @@
+'''
+Only first 4 tags will be explored. See line 41
+
+
+'''
+
+
+import pandas as pd
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+timeout = 15
+driver = webdriver.Chrome(executable_path='chromedriver')
+
+dataframe = pd.read_csv('food.csv')
+tags = [x for x in dataframe.food_name]
+
+dict_tags = {}
+
+i = 0
+for i in range(len(tags)):
+    x = tags[i]
+    url = 'https://www.instagram.com/explore/tags/' + x + '/'
+
+    driver.get(url)
+
+    try:
+        WebDriverWait(driver, timeout).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "FFVAD")))
+    except:
+        continue
+
+    image_src = driver.find_elements_by_class_name("FFVAD")
+    images = [x.get_attribute('src') for x in image_src]
+
+    dict_tags[i] = images
+    if i == 20:
+        break
+    print("--------YYYYYYAAAAAASSSSSS----------")
+
+print(len(images))
+
+driver.quit()
