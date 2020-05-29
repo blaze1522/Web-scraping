2bc4cac1b4f77493449d701e2bf9bd2b3db938d6
blaze1522 <chakrabarty.goirik@gmail.com>
2020-05-29 19:29:43 +0530

    Technically implemented xpath stucture

--- wiki-scrapping-food-1.py
+++ wiki-scrapping-food-1.py
@@ -1,70 +1,44 @@
 import requests
 import bs4
-import selenium
-from selenium import webdriver
-
-not_food_list = ['Vegetarian', 'Non-Vegetarian', 'Sweet', ]
+import pickle
 
 
-class hash_tag(object):
-    pass
-
-
-def foodname(x):
-    inputs = list(map(str, x.split()))
-    y = 0
-    if len(inputs) <= 3 & len(inputs) > 0:
-        if inputs[0] not in not_food_list:
-            if inputs[0][0] != '[':
-                y += 1
-    if y == 1:
-        return True
-    else:
-        return False
-
-
-def remove_space(x):
+def make_tags(x):
     char_list = [i for i in x]
+    # Remove spaces
     for i in char_list:
         if i == ' ':
             char_list.remove(i)
+    # Remove bracketed text
+    j = 0
+    for j in range(len(char_list)):
+        if char_list[j] in '(':
+            char_list = char_list[0: j]
+            break
+        else:
+            j += 1
     new = ""
     for s in char_list:
         new += s
-    return new
+    return new.lower()
 
 
-def scraping_wiki():
+def wikiscrap():
     res = requests.get('https://en.wikipedia.org/wiki/List_of_Indian_dishes')
     soup = bs4.BeautifulSoup(res.text, 'lxml')
 
-    foodtable = soup.select("body table tbody tr td")
-    j = 0
+    tables = soup.findAll("tbody")
     foodlist = []
+    foodname = ''
 
-    for i in foodtable:
-        j += 1
-        if foodname(i.text):
-            foodlist.append(remove_space(i.text).lower())
-        if j == 1212:
-            break
+    for table in tables[1:6]:
+        for row in table.findAll('tr')[1:]:
+            foodname = row.findAll('td')[0].text
+            foodlist.append(make_tags(foodname))
     return foodlist
 
 
-tags = scraping_wiki()
+tags = wikiscrap()
 
 print(len(tags))
-
-for i in tags:
-    print(i)
-
 print(tags)
-
-# https://www.instagram.com/explore/tags/aloogobi/
-
-
-insta_url = 'https://www.instagram.com/explore/tags/' + tags[5] + '/'
-
-# driver = webdriver.Chrome(
-#  executable_path = r'C:\Users\Goirik\AppData\Local\Google\Chrome\Application\chromedriver.exe')
-# res=driver.get(insta_url)
